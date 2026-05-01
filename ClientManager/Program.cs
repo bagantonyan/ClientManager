17e7e6edda6e8092a77fc15b820a3df9fd82b08b
Bagrat Antonyan <antonyan.b@inka.ru>
2026-05-01 18:14:10 +0300

    added ServiceExtensions

--- ClientManager/Program.cs
+++ ClientManager/Program.cs
@@ -1,3 +1,6 @@
+using ClientManager.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 namespace ClientManager
 {
     public class Program
@@ -6,17 +9,29 @@ namespace ClientManager
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-
+            builder.Services.ConfigureCors();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
 
             app.UseHttpsRedirection();
 
+            app.UseStaticFiles();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
+
+            app.MapControllers();
 
             app.Run();
         }
